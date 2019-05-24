0bd2797dbc3ffd742010557c5950a8a4071ffdd8
Cory Benfield <lukasa@apple.com>
2019-05-24 17:05:02 +0100

    Amendments for perf testing

--- Sources/NIOHTTP2Server/main.swift
+++ Sources/NIOHTTP2Server/main.swift
@@ -24,6 +24,12 @@ final class HTTP1TestServer: ChannelInboundHandler {
     public typealias InboundIn = HTTPServerRequestPart
     public typealias OutboundOut = HTTPServerResponsePart
 
+    private let streamID: HTTP2StreamID
+
+    internal init(streamID: HTTP2StreamID) {
+        self.streamID = streamID
+    }
+
     public func channelRead(context: ChannelHandlerContext, data: NIOAny) {
         guard case .end = self.unwrapInboundIn(data) else {
             return
@@ -31,21 +37,15 @@ final class HTTP1TestServer: ChannelInboundHandler {
 
         // Insert an event loop tick here. This more accurately represents real workloads in SwiftNIO, which will not
         // re-entrantly write their response frames.
-        context.eventLoop.execute {
-            context.channel.getOption(HTTP2StreamChannelOptions.streamID).flatMap { (streamID) -> EventLoopFuture<Void> in
-                var headers = HTTPHeaders()
-                headers.add(name: "content-length", value: "5")
-                headers.add(name: "x-stream-id", value: String(Int(streamID)))
-                context.channel.write(self.wrapOutboundOut(HTTPServerResponsePart.head(HTTPResponseHead(version: .init(major: 2, minor: 0), status: .ok, headers: headers))), promise: nil)
+	var headers = HTTPHeaders()
+	headers.add(name: "content-length", value: "5")
+	headers.add(name: "x-stream-id", value: String(Int(self.streamID)))
+	context.channel.write(self.wrapOutboundOut(HTTPServerResponsePart.head(HTTPResponseHead(version: .init(major: 2, minor: 0), status: .ok, headers: headers))), promise: nil)
 
-                var buffer = context.channel.allocator.buffer(capacity: 12)
-                buffer.writeStaticString("hello")
-                context.channel.write(self.wrapOutboundOut(HTTPServerResponsePart.body(.byteBuffer(buffer))), promise: nil)
-                return context.channel.writeAndFlush(self.wrapOutboundOut(HTTPServerResponsePart.end(nil)))
-            }.whenComplete { _ in
-                context.close(promise: nil)
-            }
-        }
+	var buffer = context.channel.allocator.buffer(capacity: 12)
+	buffer.writeStaticString("hello")
+	context.channel.write(self.wrapOutboundOut(HTTPServerResponsePart.body(.byteBuffer(buffer))), promise: nil)
+	context.channel.writeAndFlush(self.wrapOutboundOut(HTTPServerResponsePart.end(nil)), promise: nil)
     }
 }
 
@@ -95,7 +95,7 @@ default:
     bindTarget = BindTo.ip(host: defaultHost, port: defaultPort)
 }
 
-let group = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
+let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
 let bootstrap = ServerBootstrap(group: group)
     // Specify backlog and enable SO_REUSEADDR for the server itself
     .serverChannelOption(ChannelOptions.backlog, value: 256)
@@ -105,7 +105,7 @@ let bootstrap = ServerBootstrap(group: group)
     .childChannelInitializer { channel in
         return channel.configureHTTP2Pipeline(mode: .server) { (streamChannel, streamID) -> EventLoopFuture<Void> in
             return streamChannel.pipeline.addHandler(HTTP2ToHTTP1ServerCodec(streamID: streamID)).flatMap { () -> EventLoopFuture<Void> in
-                streamChannel.pipeline.addHandler(HTTP1TestServer())
+                streamChannel.pipeline.addHandler(HTTP1TestServer(streamID: streamID))
             }.flatMap { () -> EventLoopFuture<Void> in
                 streamChannel.pipeline.addHandler(ErrorHandler())
             }
